56cf7705090f1948b0013c4908c1d04724c5fb2a
Ryan Dodd <ryandodd@canvaspeople.com>
2018-08-21 17:43:08 -0700

    add resource manager src skeleton

--- Zombiez/src/ZombiezGame.cpp
+++ Zombiez/src/ZombiezGame.cpp
@@ -1,9 +1,11 @@
 #include "ZombiezGame.h"
-#include "Oasis/Game.h"
+#include <Oasis/Game.h>
+#include <Oasis/ResourceManager.h>
+#include <Oasis/Texture2D.h>
 
 ZombiezGame::ZombiezGame(int screenWidth, int screenHeight) : Oasis::Game("Zombiez!!", screenWidth, screenHeight)
 {
-
+	auto t = Oasis::ResourceManager::LoadTexture("resources/textures/circle.png", true, "circle");
 }
 
 ZombiezGame::~ZombiezGame()
